3ff44f081ecf6d45b940e026cb3379a8b7ea5dea
Rizwaan <rizwaanans786@gmail.com>
2024-05-25 02:00:30 +0530

    Started Bento Grid Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Image from "next/image";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
+        <Grid />
       </div>
     </main>
   );

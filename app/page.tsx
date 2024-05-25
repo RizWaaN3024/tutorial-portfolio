dac6a47c74380db78c864962165981e021c5a4af
Rizwaan <rizwaanans786@gmail.com>
2024-05-26 01:23:50 +0530

    Added FloatingNavBar and Recent Projects Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />

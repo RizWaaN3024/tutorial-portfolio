e60b634ddac056096a4f3861c936eaba89283c18
Rizwaan <rizwaanans786@gmail.com>
2024-05-28 01:14:59 +0530

    Added Experience Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -15,6 +16,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
